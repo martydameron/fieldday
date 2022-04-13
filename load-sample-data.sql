87ffd8e2513231b01c2d6bda7f1cbac7472aa1b0
angela mgbeke <101954762+amgbeke@users.noreply.github.com>
2022-04-13 10:24:41 -0400

    Create load-sample-data.sql

---
+++ load-sample-data.sql
@@ -0,0 +1,7 @@
+load data local infile 'sampledata/sample-college.csv' into table fieldday_db.college fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-sport.csv' into table fieldday_db.sport fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-game.csv' into table fieldday_db.game fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-post.csv' into table fieldday_db.post fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-user.csv' into table fieldday_db.user fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-feed.csv' into table fieldday_db.feed fields terminated by ',' ignore 1 rows;
+load data local infile 'sampledata/sample-form.csv' into table fieldday_db.form fields terminated by ',' ignore 1 rows;
